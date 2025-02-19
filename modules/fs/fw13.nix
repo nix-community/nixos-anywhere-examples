f31bf30553d5e686fadbe47a7de746aef1fa57c5
Robert <102612257+SmolPatches@users.noreply.github.com>
2025-02-19 01:40:05 +0100

    wip: framework 13 disko config
    this is made because i want to run nixos and fedora on the same
    machine
    but provide a reasonable amount of separation between the 2
    while not choosing partition sizes ahead of time

---
+++ modules/fs/fw13.nix
@@ -0,0 +1,95 @@
+# Layout 
+# | Partition       | Size  | OS     | Type      |
+# | --------------- | ----- | ------ | --------- |
+# | /boot           | 1 Gb  | Shared | fat32     |
+# | /swap           | 32 Gb | Shared | subvolume |
+# | /               | ~     | Shared | btrfs     |
+# | /nix/store      | ~     | NixOS  | subvolume |
+# | / (Fedora Root) | ~     | Fedora | subvolume |
+# | / (NixOS Root)  | ~     | NixOS  | subvolume |
+# | /home/Shared    | ~     | Shared | subvolume |
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            encryptedSwap = {
+              size = "32G";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+                priority = 100; # prefer to encrypt as long as we have space for it
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                # disable settings.keyFile if you want to use interactive password entry
+                #passwordFile = "/tmp/secret.key"; # Interactive
+                settings = {
+                  allowDiscards = true;
+                  keyFile = "/tmp/secret.key";
+                };
+                additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    # this should only be mounted if on fedora
+                    # how do i not mount this automatically
+                    "root_fedora" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    # this should only be mounted if on nixos
+                    "root_nixos" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    # shared contents between fedora and nixos
+                    "shared" = {
+                      mountpoint = "/home/Shared";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
