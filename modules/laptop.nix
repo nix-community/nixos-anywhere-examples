2914c23f5cd8c049382176a31a7ce1369c762b68
Robert <102612257+SmolPatches@users.noreply.github.com>
2025-02-20 18:46:10 +0100

    feat: nix setup for laptops copied from homelab

---
+++ modules/laptop.nix
@@ -0,0 +1,21 @@
+# this is for my custom homelab setup
+# differentiate later
+{ config, pkgs, lib }: {
+
+  users = {
+    mutableUsers = lib.mkForce false;
+    users.lain = {
+      # add HashedPassword( in sops for even more security )
+      extraGroups = [
+        "wheel"
+      ];
+      shells = pkgs.zsh;
+      isNormalUser = true;
+      openssh.authorizedKeys.keyFiles = let ssh_keys = (builtins.fetchurl { url = "https://github.com/SmolPatches.keys"; sha256 = ""; }); in [ ssh_keys ]; # point key files to the thing in nix_store
+      packages = with pkgs; [
+        eza
+        ssh-to-age
+      ];
+    };
+  };
+}
